2e188590a09d57ec3ef76cfff6da0117dc7e2be9
Keshav Dutt Kaushik <codemxde23@gmail.com>
2025-02-19 09:01:42 +0530

    completed week 4.2 assignment and expressi-fied todo application

--- week-4-nodejs-and-http/4.2-http-servers/index.js
+++ week-4-nodejs-and-http/4.2-http-servers/index.js
@@ -5,6 +5,7 @@
 const fs = require("fs");
 const filePath = "config.json";
 
+const chalk = require("chalk");
 const express = require("express");
 app = express();
 
@@ -32,6 +33,15 @@ const addTask = (task, todo) => {
   todo[taskKey] = task;
 };
 
+const updateTask = (taskKey, task, todo) => {
+  if (!Object.hasOwn(todo, taskKey)) {
+    throw new Error(
+      "Task Key does not exist!\nPlease use GET request to view all valid options\n"
+    );
+  }
+  todo[taskKey] = task;
+};
+
 const generateTaskKey = (todo) => {
   const taskKey = Object.keys(todo).length + 1;
   return "task_" + taskKey;
@@ -40,6 +50,12 @@ const generateTaskKey = (todo) => {
 //  GET call to fetch all TODOs
 app.get("/", function (req, res) {
   fs.readFile("config.json", "utf-8", (err, data) => {
+    if (err) {
+      console.log(chalk.red(err.message, "\n"));
+      res.status(500).json({ msg: "Error occured while reaading config file" });
+    }
+
+    console.log(chalk.green("GET Successful\n"));
     res.json(JSON.parse(data));
   });
 });
@@ -49,7 +65,8 @@ app.post("/", function (req, res) {
   const task = req.query.task;
   fs.readFile(filePath, "utf-8", (err, data) => {
     if (err) {
-      res.status(500).send("Error occured while reaading config file");
+      console.log(chalk.red(err.message, "\n"));
+      res.status(500).json({ err: "Error occured while reaading config file" });
     }
 
     const todo = JSON.parse(data);
@@ -57,17 +74,87 @@ app.post("/", function (req, res) {
 
     fs.writeFile(filePath, JSON.stringify(todo, null, 2), (err) => {
       if (err) {
-        console.log("Error occurred while writing config file");
+        console.log(chalk.red(err.message, "\n"));
         res.status(500).json({
-          msg: "Failed to write file on disc",
+          err: "Failed to write file on disc",
         });
       }
 
+      console.log(chalk.green("Succesfully Added Todo\n"));
       res.status(200).json({
         msg: "Succesfully Added Todo",
       });
     });
   });
+});
+
+// PUT call to update a TODO based on task key
+app.put("/", function (req, res) {
+  const taskKey = req.query.taskKey;
+  const task = req.query.task;
+
+  fs.readFile(filePath, "utf-8", function (err, data) {
+    if (err) {
+      console.log(chalk.red(err.message));
+      res.status(500).send("Error occured while reaading config file");
+    }
+    const todo = JSON.parse(data);
+    try {
+      updateTask(taskKey, task, todo);
+      fs.writeFile(filePath, JSON.stringify(todo, null, 2), (err) => {
+        if (err) {
+          throw new Error("Error occured while writing config file");
+        } else {
+          console.log(chalk.green("Updated TODO Successfully"));
+          res.status(200).send("Updated TODO Successfully");
+        }
+      });
+    } catch (err) {
+      console.log(chalk.red(err.message));
+      res
+        .status(500)
+        .json({ err: "Unexpected Error While Trying to Update Task" });
+    }
+  });
+});
+
+// DELETE call to remove a TODO based on task key
+app.delete("/", function (req, res) {
+  try {
+    const { taskKey } = req.query;
+    if (!taskKey) {
+      console.log(chalk.red("Requested Parameter Not Sent\n"));
+      res.status(400).send("Requested Parameter Not Sent");
+      return;
+    }
+
+    fs.readFile(filePath, "utf-8", (err, data) => {
+      if (err) {
+        throw new Error("Error occured while writing config file");
+      }
+
+      const todo = JSON.parse(data);
+      if (!Object.hasOwn(todo, taskKey)) {
+        // throw new Error("Key Provided is Invalid");
+        console.log(chalk.red("Key Provided is Invalid\n"));
+        res.status(400).send("Key Provided is Invalid");
+        return;
+      }
+
+      delete todo[taskKey];
+
+      fs.writeFile(filePath, JSON.stringify(todo, null, 2), (err) => {
+        if (err) {
+          throw new Error("Error Occurred Trying to Delete");
+        }
+        console.log(chalk.green("Task Delete Successfull\n"));
+        res.status(200).send("Task Delete Successfull");
+      });
+    });
+  } catch (error) {
+    console.log(chalk.red(error));
+    res.status(500).json({ err: error.message });
+  }
 });
 
 app.listen("3000");
