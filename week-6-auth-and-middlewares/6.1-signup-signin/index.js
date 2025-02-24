91ceb00dc409ab96e9eda6d4f6d0daa1a7314592
Keshav Dutt Kaushik <codemxde23@gmail.com>
2025-02-24 16:44:52 +0530

    build a short and simple auth flow for week-6.1 assignment

---
+++ week-6-auth-and-middlewares/6.1-signup-signin/index.js
@@ -0,0 +1,76 @@
+const express = require("express");
+const app = express();
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = "codemxde";
+const cors = require("cors");
+
+const users = [];
+
+const extractUserInfo = (username) => {
+  return users.find((user) => user.username === username);
+};
+
+app.use(cors());
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/validate-token", (req, res) => {
+  const token = req.headers.authorization;
+  if (token) {
+    try {
+      const decodedInfo = jwt.verify(token, JWT_SECRET);
+      const username = decodedInfo.username;
+      const user = extractUserInfo(username);
+      if (user) {
+        res.status(200).json(user);
+      } else {
+        res.status(403).json({ error: "sign in again" });
+      }
+    } catch (error) {
+      // * token is invalid, redirect to signup/signin
+      res.status(403).json({
+        error: "user is not authorized! Please Sign-up/Sign-in",
+      });
+    }
+  }
+});
+
+app.post("/signup", (req, res) => {
+  const { username, password } = req.body;
+  const user = extractUserInfo(username);
+
+  if (user) {
+    res.status(409).json({
+      error: "user already exists",
+    });
+  } else {
+    users.push({ username, password });
+    res.status(200).json({ success: "user created" });
+  }
+});
+
+app.post("/signin", (req, res) => {
+  const { username, password } = req.body;
+  const user = extractUserInfo(username);
+
+  if (!user) {
+    //  send 404 user not found, tell user to sign up first
+    res.status(404).json({ error: "user not found. you should sign up first" });
+  } else if (user.username === username && user.password === password) {
+    //  generate jwt token
+    const token = jwt.sign({ username }, JWT_SECRET);
+    res.setHeader("Authentication", token);
+    // Expose the custom header so that the client can access it
+    res.setHeader("Access-Control-Expose-Headers", "Authentication");
+    res.status(200).json({ success: "user is verified" });
+  } else if (user.username === username && user.password !== password) {
+    //  send 403 unauthorized: incorrect password
+    res.status(403).json({ error: "incorrect password" });
+  }
+});
+
+app.listen(3000);
---
+++ week-6-auth-and-middlewares/6.1-signup-signin/public/script.js
@@ -0,0 +1,225 @@
+let userInfo = null;
+
+const body = document.body;
+const root = document.getElementById("root");
+const divContainer = document.createElement("div");
+
+const description = document.createElement("h2");
+const signUpContainer = document.createElement("div");
+const signInContainer = document.createElement("div");
+
+const signUpUsername = document.createElement("input");
+const signUpPassword = document.createElement("input");
+const signUpBtn = document.createElement("button");
+
+const signInUsername = document.createElement("input");
+const signInPassword = document.createElement("input");
+const signInBtn = document.createElement("button");
+
+const userContainer = document.createElement("div");
+const logoutBtn = document.createElement("button");
+const welcome = document.createElement("h1");
+
+const welcomeText = document.createElement("p");
+welcomeText.textContent =
+  "Hey, we have something in common! We both seem to be tech junkies! We get all our dopamine from it, frankly.\n Our netcafe is the best when it comes to the latest news, trends and masala when it comes to tech!";
+
+const addSignUpListener = () => {
+  signUpBtn.addEventListener("click", () => {
+    const username = signUpUsername.value;
+    const password = signUpPassword.value;
+
+    clearSignUpFields();
+
+    fetch("http://localhost:3000/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username, password }),
+    })
+      .then((response) => {
+        if (response.status === 200) {
+          alert("Sign up Successful!");
+        } else if (response.status === 409) {
+          alert(
+            `${username} is already signed up! you should sign in instead.`
+          );
+        }
+      })
+      .catch((err) => {
+        // alert(err.message);
+        alert("Server refused connection");
+      });
+  });
+};
+
+const clearSignUpFields = () => {
+  signUpUsername.value = "";
+  signUpPassword.value = "";
+};
+
+const addSigninListener = () => {
+  signInBtn.addEventListener("click", () => {
+    const username = signInUsername.value;
+    const password = signInPassword.value;
+
+    clearSignInFields();
+
+    fetch("http://localhost:3000/signin", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username, password }),
+    })
+      .then((response) => {
+        if (response.status === 200) {
+          const token = response.headers.get("Authentication");
+          if (token) {
+            localStorage.setItem("token", token);
+            loadDOM();
+          } else {
+            alert("Server did not provide auth token!");
+          }
+        } else if (response.status === 404) {
+          alert("user is not signed up yet");
+        } else if (response.status === 403) {
+          alert("password is incorrect!");
+        }
+      })
+      .catch((err) => {
+        alert("server connection aborted!");
+      });
+  });
+};
+
+const clearSignInFields = () => {
+  signInUsername.value = "";
+  signInPassword.value = "";
+};
+
+const createAuthComponent = () => {
+  divContainer.className = "auth-container";
+  description.className = "description-header";
+  signUpContainer.className = "sign-up-container";
+  signInContainer.className = "sign-up-container";
+
+  description.textContent = "welcome to codemxde.com";
+
+  signUpUsername.id = "sign-up-username";
+  signUpUsername.type = "text";
+  signUpUsername.placeholder = "username";
+  signUpPassword.id = "sign-up-password";
+  signUpPassword.type = "password";
+  signUpPassword.placeholder = "password";
+  signUpBtn.textContent = "Sign Up";
+  signUpBtn.className = "sign-btn";
+  addSignUpListener();
+
+  signUpContainer.appendChild(signUpUsername);
+  signUpContainer.appendChild(signUpPassword);
+  signUpContainer.appendChild(signUpBtn);
+
+  signInUsername.id = "sign-in-username";
+  signInUsername.type = "text";
+  signInUsername.placeholder = "username";
+  signInPassword.id = "sign-in-password";
+  signInPassword.type = "password";
+  signInPassword.placeholder = "password";
+  signInBtn.textContent = "Sign In";
+  signInBtn.className = "sign-btn";
+  addSigninListener();
+
+  signInContainer.appendChild(signInUsername);
+  signInContainer.appendChild(signInPassword);
+  signInContainer.appendChild(signInBtn);
+
+  divContainer.appendChild(description);
+  divContainer.appendChild(signUpContainer);
+  divContainer.appendChild(signInContainer);
+
+  divContainer.classList.add("hide");
+
+  root.appendChild(divContainer);
+};
+
+createAuthComponent();
+
+const addLogoutListener = () => {
+  logoutBtn.addEventListener("click", () => {
+    localStorage.removeItem("token");
+    // hide welcome container
+    userContainer.style.display = "none";
+    body.classList.remove("login-img");
+    loadDOM();
+  });
+};
+
+const createLogout = () => {
+  logoutBtn.className = "logout-btn";
+  logoutBtn.textContent = "Log Out";
+
+  addLogoutListener();
+};
+
+createLogout();
+
+const createWelcomeComponent = () => {
+  welcome.classList.add("welcome-header");
+  welcomeText.className = "welcome-text";
+  createLogout();
+  userContainer.appendChild(welcome);
+  userContainer.appendChild(welcomeText);
+  userContainer.appendChild(logoutBtn);
+  userContainer.className = "user-container";
+  userContainer.style.display = "none";
+
+  root.appendChild(userContainer);
+};
+
+createWelcomeComponent();
+
+function loadDOM() {
+  // * check whether there is a jwt token present in the local storage
+  const token = localStorage.getItem("token");
+  if (token) {
+    // * first verify token from BE
+    fetch("http://localhost:3000/validate-token", {
+      method: "POST",
+      headers: {
+        Authorization: token,
+      },
+    })
+      .then((response) => {
+        if (response.status === 200) {
+          return response.json();
+        } else if (response.status === 403) {
+          localStorage.removeItem("token", token);
+          alert("please sign in again");
+          divContainer.classList.remove("hide");
+          document.body.classList.add("render-img");
+        }
+      })
+      .then((data) => {
+        if (data) {
+          console.log(data);
+          userInfo = data;
+          divContainer.classList.add("hide");
+          body.classList.remove("render-img");
+          body.classList.add("login-img");
+
+          welcome.textContent = `Welcome ${userInfo.username}!`;
+          userContainer.style.display = "flex";
+        }
+      })
+      .catch((err) => {
+        alert("Server refused connection");
+      });
+  } else {
+    // * render the signin and sign up components
+    document.body.classList.add("render-img");
+    divContainer.classList.remove("hide");
+  }
+}
+loadDOM();
